9bd7c887f660d07d7295dad6eca555c6b1b0bc38
Sam <samyankun@gmail.com>
2018-11-22 11:31:47 +0800

    add nodejs-mobile-react-native support

---
+++ nodejs-assets/build-native-modules-MacOS-helper-script-npm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+      # Helper script for Gradle to call npm on macOS in case it is not found
+      export PATH=$PATH:/Users/sam/.nvm/versions/node/v8.12.0/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/sam/Public/trustpocket/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/sam/Public/trustpocket/node_modules/.bin:/Users/sam/.nvm/versions/node/v8.12.0/bin:/usr/local/eosio/bin/:/Users/sam/.jenv/shims:/Users/sam/.jenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin:/Applications/Android Studio.app/Contents/gradle/gradle-4.4/bin:/Volumes/Sam/Android/sdk/tools:/Volumes/Sam/Android/sdk/platform-tools
+      npm $@
+    
