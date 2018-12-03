96981acc1081c36d7887fdb80248bd412e86912c
liulinnan <liulinnan@ulaidao.com>
2018-12-03 18:30:42 +0800

    [Style] Add common style
    supply public style.
    
    Signed-off-by: liulinnan@ulaidao.com
    Acked-by: yankun@ulaidao.com

---
+++ app/style/commonStyle.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+const commonStyles = StyleSheet.create({
+    container: {
+        color: '#fff',
+        flexDirection: 'column',
+        backgroundColor: '#999',
+        padding: 20,
+        flex: 1
+    },
+});
+
+module.exports = commonStyles;
