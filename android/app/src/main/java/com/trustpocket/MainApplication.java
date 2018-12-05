76447f305cb6b170c238d35768ed8297205b85b4
hjn <houjingnan@ulaidao.com>
2018-12-05 14:40:54 +0800

    Android configuration for camera
    some configuration for camera running normal
    
    Signed-off-by:houjingnan@ulaidao.com
    Acked-by: yankun@ulaidao.com

--- android/app/src/main/java/com/trustpocket/MainApplication.java
+++ android/app/src/main/java/com/trustpocket/MainApplication.java
@@ -3,11 +3,13 @@ package com.trustpocket;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.janeasystems.rn_nodejs_mobile.RNNodeJsMobilePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import org.reactnative.camera.RNCameraPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,7 +24,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNCameraPackage(),
+            new RNGestureHandlerPackage(),
+            new RNNodeJsMobilePackage()
       );
     }
 
